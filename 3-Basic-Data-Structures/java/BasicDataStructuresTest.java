e5d9ba440648fc78adba64000f323bdb384b3837
Mihail Mihaylov <mihailm@vmware.com>
2020-07-17 17:50:56 +0300

    Add java implementation for doubly linked list.

--- 3-Basic-Data-Structures/java/BasicDataStructuresTest.java
+++ 3-Basic-Data-Structures/java/BasicDataStructuresTest.java
@@ -22,4 +22,22 @@ public class BasicDataStructuresTest {
         list.eraseEnd();
         Assert.assertEquals("[5, 4]", list.toString());
     }
+
+    @Test
+    public void testDoublyLinkedList() {
+        DoublyLinkedList<Integer> list = new DoublyLinkedList<>();
+
+        list.addToBeginning(1);
+        list.addToEnd(5);
+        list.insertAt(4,1);
+        list.insertAt(19, 2);
+        Assert.assertEquals("[1, 4, 19, 5]", list.toString());
+
+        list.reverse();
+        Assert.assertEquals("[5, 19, 4, 1]", list.toString());
+
+        list.eraseAt(1);
+        list.eraseEnd();
+        Assert.assertEquals("[5, 4]", list.toString());
+    }
 }

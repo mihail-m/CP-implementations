63cc8fd82443ef281794790dda6a43d688fb9cbd
mihail-m <momodei.misho@gmail.com>
2020-07-23 15:48:34 +0300

    Add Edmonds-Karp c++ implementation.

--- 4-Graphs/01-Fundamentals/c++/13-edmonds-karp.cpp
+++ 4-Graphs/01-Fundamentals/c++/13-edmonds-karp.cpp
@@ -8,78 +8,78 @@ vector<int> graph[MAX_SIZE];
 int capacity[MAX_SIZE][MAX_SIZE];
 
 int bfs(int from, int to, vector<int> parent) {
-	fill(parent.begin(), parent.end(), -1);
-	parent[from] = INT_MIN;
+    fill(parent.begin(), parent.end(), -1);
+    parent[from] = INT_MIN;
 
-	queue<pair<int, int>> q;
-	q.push({from, INT_MAX});
+    queue<pair<int, int>> q;
+    q.push({from, INT_MAX});
 
-	while (!q.empty()) {
-		int node = q.front().first;
-		int flow = q.front().second;
-		q.pop();
+    while (!q.empty()) {
+        int node = q.front().first;
+        int flow = q.front().second;
+        q.pop();
 
-		for (int i : graph[node]) {
-			if (parent[i] == -1 && capacity[node][i]) {
-				parent[i] = node;
-				int current_flow = min(flow, capacity[node][i]);
+        for (int i : graph[node]) {
+            if (parent[i] == -1 && capacity[node][i]) {
+                parent[i] = node;
+                int current_flow = min(flow, capacity[node][i]);
 
-				if (i == to) {
-					return current_flow;
-				}
+                if (i == to) {
+                    return current_flow;
+                }
 
-				q.push({i, current_flow});
-			}
-		}
-	}
+                q.push({i, current_flow});
+            }
+        }
+    }
 
-	return 0;
+    return 0;
 }
 
 int max_flow(int source, int sink) {
-	int flow = 0;
-	int current_flow = 0;
-	vector<int> parent(n);
+    int flow = 0;
+    int current_flow = 0;
+    vector<int> parent(n);
 
-	while (current_flow = bfs(source, sink, parent)) {
-		flow += current_flow;
+    while (current_flow = bfs(source, sink, parent)) {
+        flow += current_flow;
 
-		int node = sink;
-		while (node != source) {
-			int previous = parent[node];
-			capacity[previous][node] -= current_flow;
-			capacity[node][previous] += current_flow;
-			node = previous;
-		}
-	}
+        int node = sink;
+        while (node != source) {
+            int previous = parent[node];
+            capacity[previous][node] -= current_flow;
+            capacity[node][previous] += current_flow;
+            node = previous;
+        }
+    }
 
-	return flow;
+    return flow;
 }
 
 void input() {
-	cin >> n >> m;
-	for (int i = 0; i < m; i++) {
-		int from, to, cap;
-		cin >> from >> to >> cap;
-		graph[from].push_back(to);
-		capacity[from][to] = cap;
-	}
+    cin >> n >> m;
+    for (int i = 0; i < m; i++) {
+        int from, to, cap;
+        cin >> from >> to >> cap;
+        graph[from].push_back(to);
+        capacity[from][to] = cap;
+    }
 }
 
 void test() {
-	input();
+    input();
 
-	int source, sink;
-	cin >> source >> sink;
-	cerr << max_flow(source, sink) << "\n";
+    int source, sink;
+    cin >> source >> sink;
+    cerr << max_flow(source, sink) << "\n";
 }
 
 int main() {
-	ios_base::sync_with_stdio(false);
-	cin.tie(nullptr);
-	cout.tie(nullptr);
+    ios_base::sync_with_stdio(false);
+    cin.tie(nullptr);
+    cout.tie(nullptr);
 
-	test();
+    test();
 
-	return 0;
+    return 0;
 }

e7f20dd21e96062943adb2e2b3c459e20ff85d81
mihail-m <momodei.misho@gmail.com>
2020-07-24 14:45:03 +0300

    Add c++ implementation for Dinic.

--- 4-Graphs/02-Advanced/07-dinic.cpp
+++ 4-Graphs/02-Advanced/07-dinic.cpp
@@ -1,28 +1,166 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-struct NetworkEdge {
-    int from;
-    int to;
+class Dinic {
+    private:
+        struct NetworkEdge {
+            int from;
+            int to;
 
-    long long capacity;
-    long long flow = 0;
+            long long capacity;
+            long long flow = 0;
 
-    NetworkEdge(int v, int u, long long cap) : v(v), u(u), cap(cap) {}
+            NetworkEdge(int from, int to, long long capacity) : from(from), to(to), capacity(capacity) {}
+        };
+
+        int n;
+        int m;
+
+        vector<NetworkEdge> edges;
+        vector<vector<int>> graph;
+
+        int source;
+        int sink;
+
+        vector<int> level;
+        vector<int> ptr;
+
+        queue<int> q;
+
+        bool bfs() {
+            while (!q.empty()) {
+                int node = q.front();
+                q.pop();
+
+                for (int index : graph[node]) {
+                    NetworkEdge edge = edges[index];
+
+                    if (edge.capacity - edge.flow < 1) {
+                        continue;
+                    }
+
+                    if (level[edge.to] != -1) {
+                        continue;
+                    }
+
+                    level[edge.to] = level[node] + 1;
+                    q.push(edge.to);
+                }
+            }
+
+            return level[sink] != -1;
+        }
+
+        long long dfs(int node, long long pushed) {
+            if (pushed == 0) {
+                return 0;
+            }
+
+            if (node == sink) {
+                return pushed;
+            }
+
+            for (ptr[node]; ptr[node] < graph[node].size(); ptr[node]++) {
+                int id = graph[node][ptr[node]];
+                NetworkEdge& edge = edges[id];
+                NetworkEdge& reverse_edge = edges[id ^ 1];
+
+                if (level[node] + 1 != level[edge.to] || edge.capacity - edge.flow < 1) {
+                    continue;
+                }
+
+                long long current_flow = dfs(edge.to, min(pushed, edge.capacity - edge.flow));
+
+                if (current_flow == 0) {
+                    continue;
+                }
+
+                edge.flow += current_flow;
+                reverse_edge.flow -= current_flow;
+
+                return current_flow;
+            }
+
+            return 0;
+        }
+
+        void reset() {
+            while (!q.empty()) {
+                q.pop();
+            }
+
+            for (NetworkEdge& edge : edges) {
+                edge.flow = 0;
+            }
+        }
+
+    public:
+        Dinic(int n) : n(n), m(0) {
+            graph.resize(n);
+            level.resize(n);
+            ptr.resize(n);
+        }
+
+        void add_edge(int from, int to, long long cap) {
+            edges.push_back({from, to, cap});
+            edges.push_back({to, from, 0});
+
+            graph[from].push_back(m++);
+            graph[to].push_back(m++);
+        }
+
+        long long max_flow(int from, int to) {
+            reset();
+
+            source = from;
+            sink = to;
+
+            long long flow = 0;
+
+            while (true) {
+                fill(level.begin(), level.end(), -1);
+                level[source] = 0;
+                q.push(source);
+
+                if (!bfs()) {
+                    break;
+                }
+
+                fill(ptr.begin(), ptr.end(), 0);
+
+                long long pushed = dfs(source, LLONG_MAX);
+                while (pushed > 0) {
+                    flow += pushed;
+                    pushed = dfs(source, LLONG_MAX);
+                }
+            }
+
+            return flow;
+        }
 };
 
-class Dinic {
-	private:
-		int n;
-		int m;
+void test() {
+    Dinic dinic(6);
+    dinic.add_edge(0, 1, 16);
+    dinic.add_edge(0, 2, 13);
+    dinic.add_edge(1, 2, 10);
+    dinic.add_edge(1, 3, 12);
+    dinic.add_edge(2, 1, 4);
+    dinic.add_edge(2, 4, 14);
+    dinic.add_edge(3, 2, 9);
+    dinic.add_edge(3, 5, 20);
+    dinic.add_edge(4, 3, 7);
+    dinic.add_edge(4, 5, 4);
 
-		vector<NetworkEdge> edges;
-		vector<vector<int>> graph;
+    assert(23 == dinic.max_flow(0, 5));
+}
 
-		int source;
-		int sink;
+int main() {
+    ios_base::sync_with_stdio(false);
+    cin.tie(nullptr);
+    cout.tie(nullptr);
 
+    test();
 
-		vector<int> level;
-		vector<int> 
+    return 0;
 }

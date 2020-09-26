66da526b672e2bba13ef331aafb87c3e81151792
mihail-m <momodei.misho@gmail.com>
2020-09-26 21:38:01 +0300

    Add c++ Suffix Array implementation

--- 5-String-Matching/6-suffix-array.cpp
+++ 5-String-Matching/6-suffix-array.cpp
@@ -13,62 +13,62 @@ int suffix_array[SIZE];
 pair<pair<int,int>, int> L[SIZE];
 
 void build_suffix_array(const string& s) {
-	for(int i = 0; i < n; i++) {
-		P[0][i] = s[i];
-	}
-		
-	suffix_array[0] = 0; 
-	for(stp = 1, cnt = 1; cnt < n; stp++, cnt <<= 1) {
-		for(int i = 0; i < n; i++) {
-			if (i + cnt < n) {
-				L[i] = {{P[stp - 1][i], P[stp - 1][i + cnt]}, i};
-			} else {
-				L[i] = {{P[stp - 1][i], -1}, i};
-			}
-		}
-			
-		sort(L, L + n);
+    for(int i = 0; i < n; i++) {
+        P[0][i] = s[i];
+    }
+        
+    suffix_array[0] = 0; 
+    for(stp = 1, cnt = 1; cnt < n; stp++, cnt <<= 1) {
+        for(int i = 0; i < n; i++) {
+            if (i + cnt < n) {
+                L[i] = {{P[stp - 1][i], P[stp - 1][i + cnt]}, i};
+            } else {
+                L[i] = {{P[stp - 1][i], -1}, i};
+            }
+        }
+            
+        sort(L, L + n);
 
-		for(int i = 0; i < n; i++) {
-			if (i > 0 && L[i].first == L[i - 1].first) {
-				P[stp][L[i].second] = P[stp][L[i - 1].second];
-			} else {
-				P[stp][L[i].second] = i;
-			}
-		}
-	}
-		
-	for(int i = 0; i < n; i++) {
-		suffix_array[i] = L[i].second;
-	}
+        for(int i = 0; i < n; i++) {
+            if (i > 0 && L[i].first == L[i - 1].first) {
+                P[stp][L[i].second] = P[stp][L[i - 1].second];
+            } else {
+                P[stp][L[i].second] = i;
+            }
+        }
+    }
+        
+    for(int i = 0; i < n; i++) {
+        suffix_array[i] = L[i].second;
+    }
 }
 
 // Longest common prefix of substrings x and y
 int lcp(int x, int y) {
-	if (x == y) {
-		return n - x;
-	}
+    if (x == y) {
+        return n - x;
+    }
 
-	int ret = 0;
-	for (int k = stp - 1; k >= 0 && x < n && y < n; k--) {
-		if (P[k][x] == P[k][y]) {
-			x += (1 << k);
-			y += (1 << k);
-			ret += (1 << k);
-		}
-	}
-	
-	return ret;
+    int ret = 0;
+    for (int k = stp - 1; k >= 0 && x < n && y < n; k--) {
+        if (P[k][x] == P[k][y]) {
+            x += (1 << k);
+            y += (1 << k);
+            ret += (1 << k);
+        }
+    }
+    
+    return ret;
 }
 
 void test() {
-	string s = "aabaabaa";
+    string s = "aabaabaa";
 
-	n = s.size();
+    n = s.size();
 
-	build_suffix_array(s);
+    build_suffix_array(s);
 
-	assert(5 == lcp(0, 3));
+    assert(5 == lcp(0, 3));
 }
 
 int main() {
@@ -78,5 +78,5 @@ int main() {
 
     test();
 
-	return 0;
+    return 0;
 }
